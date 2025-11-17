d5a0a7cfa3f29882de4b0af49a9fafd030329253
Infoweb-2022 <infoweb.computaria@gmail.com>
2025-11-17 12:13:23 +0000

    Remoção arquivos do template

--- README.md
+++
@@ -1,1 +0,0 @@
-# Computaria.db
--- accounts/forms/__init__.py
+++
@@ -1,5 +0,0 @@
-from .CustomUserChangeForm import CustomUserChangeForm
-from .CustomUserCreationForm import CustomUserCreationForm
-
-from .LoginForm import LoginForm
-from .SignupForm import SignupForm
